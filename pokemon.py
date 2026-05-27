dc790a648d6d2bd5f64112aa74b30783ea64b410
Maxjacobr <mjrubenstein@cps.edu>
2026-05-27 11:32:35 -0500

    Add files via upload

---
+++ pokemon.py
@@ -0,0 +1,234 @@
+#Max Rubenstein
+#Pokemon game
+
+import random
+
+
+level = 0
+name = "Charmander"
+day = 1
+
+
+def charmander():
+    print((r"             _.--\"\"`-..\n"))
+    print((r"           ,'          `.\n"))
+    print((r"         ,'          __  `.\n"))
+    print((r"        /|          \" __   \\\n"))
+    print((r"       , |           / |.   .\n"))
+    print((r"       |,'          !_.'|   |\n"))
+    print((r"     ,'             '   |   |\n"))
+    print((r"    /              |`--'|   |\n"))
+    print((r"   |                `---'   |\n"))
+    print((r"    .   ,                   |                       ,\".\n"))
+    print((r"     ._     '           _'  |                    , ' \\ `\n"))
+    print((r" `.. `.`-...___,...---\"\"    |       __,.        ,`\"   L,|\n"))
+    print((r" |, `- .`._        _,-,.'   .  __.-'-. /        .   ,    \\\n"))
+    print(("-:..     `. `-..--_.,.<       `\"      / `.        `-/ |   .\n"))
+    print((r" `,         \"\"\"\"'     `.              ,'         |   |  ',,\n"))
+    print((r"   `.      '            '            /          '    |'. |/\n"))
+    print((r"     `.   |              \\       _,-'           |       ''\n"))
+    print((r"       `._'               \\   '\"\\                .      |\n"))
+    print((r"          |                '     \\                `._  ,'\n"))
+    print((r"          |                 '     \\                 .'|\n"))
+    print((r"          |                 .      \\                | |\n"))
+    print((r"          |                 |       L              ,' |\n"))
+    print((r"          `                 |       |             /   '\n"))
+    print((r"           \\                |       |           ,'   /\n"))
+    print((r"         ,' \\               |  _.._ ,-..___,..-'    ,'\n"))
+    print((r"        /     .             .      `!             ,j'\n"))
+    print((r"       /       `.          /        .           .'/\n"))
+    print((r"      .          `.       /         |        _.'.'\n"))
+    print((r"       `.          7`'---'          |------\"'_.'\n"))
+    print((r"      _,.`,_     _'                ,''-----\"'\n"))
+    print((r"  _,-_    '       `.     .'      ,\\\n"))
+    print((r"  -\" /`.         _,'     | _  _  _.|\n"))
+    print((r"   \"\"--'---\"\"\"\"\"'        `' '! |! /\n"))
+    print((r"                           `\" \" -' mh\n"))
+    print(("\n"))
+    print(("\n"))
+
+def charmeleon():
+    print((r"                     ,-'`\\\n"))
+    print((r"                 _,\"'    j\n"))
+    print((r"          __....+       /               .\n"))
+    print((r"      ,-'\"             /               ; `-._.'.\n"))
+    print((r"     /                (              ,'       .'\n"))
+    print((r"    |            _.    \\             \\   ---._ `-.\n"))
+    print((r"    ,|    ,   _.'  Y    \\             `- ,'   \\   `.`.\n"))
+    print((r"    l'    \\ ,'._,\\ `.    .              /       ,--. l\n"))
+    print((r" .,-        `._  |  |    |              \\       _   l .\n"))
+    print((r"/              `\"--'    /              .'       ``. |  )\n"))
+    print((".\\    ,                 |                .        \\ `. '\n"))
+    print(("`.                .     |                '._  __   ;. \\'\n"))
+    print((r" `-..--------...'       \\                  `'  `-\"'.  \\\n"))
+    print((r"     `......___          `._                        |  \\\n"))
+    print((r"              /`            `..                     |   .\n"))
+    print((r"             /|                `-.                  |    L\n"))
+    print((r"            / |               \\   `._               .    |\n"))
+    print((r"          ,'  |,-\"-.   .       .     `.            /     |\n"))
+    print((r"        ,'    |     '   \\      |       `.         /      |\n"))
+    print((r"      ,'     /|       \\  .     |         .       /       |\n"))
+    print((r"    ,'      / |        \\  .    +          \\    ,'       .'\n"))
+    print((r"   .       .  |         \\ |     \\          \\_,'        / j\n"))
+    print((r"   |       |  L          `|      .          `        ,' '\n"))
+    print((r"   |    _. |   \\          /      |           .     .' ,'\n"))
+    print((r"   |   /  `|    \\        .       |  /        |   ,' .'\n"))
+    print((r"   |   ,-..\\     -.     ,        | /         |,.' ,'\n"))
+    print((r"   `. |___,`    /  `.   /`.       '          |  .'\n"))
+    print((r"     '-`-'     j     ` /.\"7-..../|          ,`-'\n"))
+    print((r"               |        .'  / _/_|          .\n"))
+    print((r"               `,       `\"'/\"'    \\          `.\n"))
+    print((r"                 `,       '.       `.         |\n"))
+    print((r"            __,.-'         `.        \\'       |\n"))
+    print((r"           /_,-'\\          ,'        |        _.\n"))
+    print((r"            |___.---.   ,-'        .-':,-\"`\\,' .\n"))
+    print((r"                 L,.--\"'           '-' |  ,' `-.\\\n"))
+    print((r"                                       `.' mh\n"))
+
+def charzard():
+    print((r"                .\"-,.__\n"))
+    print((r"                `.     `.  ,\n"))
+    print((r"             .--'  .._,'\"-' `.\n"))
+    print((r"            .    .'         `'\n"))
+    print((r"            `.   /          ,'\n"))
+    print((r"              `  '--.   ,-\"'\n"))
+    print((r"               `\"`   |  \\\n"))
+    print((r"                  -. \\, |\n"))
+    print((r"                   `--Y.'      ___.\n"))
+    print((r"                        \\     L._, \\\n"))
+    print((r"              _.,        `.   <  <\\                _\n"))
+    print((r"            ,' '           `, `.   | \\            ( `\n"))
+    print((r"         ../, `.            `  |    .\\`.           \\ \\_\n"))
+    print((r"        ,' ,..  .           _.,'    ||\\l            )  '\".\n"))
+    print((r"       , ,'   \\           ,'.-.`-._,'  |           .  _._`.\n"))
+    print((r"     ,' /      \\ \\        `' ' `--/   | \\          / /   ..\\\n"))
+    print((r"   .'  /        \\ .         |\\__ - _ ,'` `        / /     `.`.\n"))
+    print((r"   |  '          ..         `-...-\"  |  `-'      / /        . `.\n"))
+    print((r"   | /           |L__           |    |          / /          `. `.\n"))
+    print((r"  , /            .   .          |    |         / /             ` `\n"))
+    print((r" / /          ,. ,`._ `-_       |    |  _   ,-' /               ` \\\n"))
+    print((r"/ .           \\\"`_/. `-_ \\_,.  ,'    +-' `-'  _,        ..,-.    \\`.\n"))
+    print((".  '         .-f    ,'   `    '.       \\__.---'     _   .'   '     \\ \\\n"))
+    print(("' /          `.'    l     .' /          \\..      ,_|/   `.  ,'`     L`\n"))
+    print(("|'      _.-\"\"` `.    \\ _,'  `            \\ `.___`.'\"`-.  , |   |    | \\\n"))
+    print(("||    ,'      `. `.   '       _,...._        `  |    `/ '  |   '     .|\n"))
+    print(("||  ,'          `. ;.,.---' ,'       `.   `.. `-'  .-' /_ .'    ;_   ||\n"))
+    print(("|| '              V      / /           `   | `   ,'   ,' '.    !  `. ||\n"))
+    print(("||/            _,-------7 '              . |  `-'    l         /    `||\n"))
+    print((". |          ,' .-   ,' ||               | .-.        `.      .'     ||\n"))
+    print((r"`'        ,'    `\".'    |               |    `.        '. -.'       `'\n"))
+    print((r"         /      ,'      |               |,'    \\-.._,.'/'\n"))
+    print((r"         .     /        .               .       \\    .''\n"))
+    print((r"       .`.    |         `.             /         :_,'.'\n"))
+    print((r"         \\ `...\\   _     ,'-.        .'         /_.-'\n"))
+    print((r"          `-.__ `,  `'   .  _.>----''.  _  __  /\n"))
+    print((r"               .'        /\"'          |  \"'   '_\n"))
+    print((r"              /_|.-'\\ ,\".             '.'`__'-( \\\n"))
+    print((r"                / ,\"'\"\\,'               `/  `-.|\" mh\n"))
+def train():
+    global level
+    print(f"\n {name} did ten pushups.")
+    level = level + 1
+    print("Training complete.")
+    print("Current level:", level)
+
+
+def rest():
+    print("\nYour Pokémon is resting.")
+    print("----- Pokémon Info -----")
+    print("Name :", name)
+    print("Level:", level)
+    print("------------------------")
+
+
+def gym_battle():
+    global level
+    print("\nYou enter a gym battle.")
+    outcome = random.randint(1, 2)
+
+    if outcome == 1:
+        print("Your Pokémon won the battle.")
+        level = level + 2
+        print("Level increased to:", level)
+    else:
+        print("Your Pokémon lost the battle.")
+        print("Level stays the same.")
+
+def final_boss_battle():
+    global level
+    if level >= 36:
+        outcome = random.randint(1,4)
+        if outcome == 1:
+            print("You have officially beaten the final boss!!!")
+            print("Continue training if you would like")
+        else:
+            print()
+            print("After a long hard battle, you lost! Keep training and try again!")
+    elif level >= 16:
+        outcome = random.randint(1,10)
+        if outcome == 1:
+            print("You have officially beaten the final boss!!!")
+            print("Continue training if you would like")
+        else:
+            print()
+            print("After a long hard battle, you lost! Keep training and try again!")
+    else:
+        outcome = random.randint(1,25)
+        if outcome == 1:
+            print("You have officially beaten the final boss!!!")
+            print("Continue training if you would like")
+        else:
+            print()
+            print("After a long hard battle, you lost! Keep training and try again!")
+def evolve():
+    global name, level
+
+    if level >= 16:
+        print("\nYour Pokémon is evolving.")
+        name = "Charmeleon"
+        print("Your Pokémon evolved into Charmeleon.")
+        charmeleon()
+    elif level >= 35:
+        print("\nYour Pokémon is evolving.")
+        name = "Charizard"
+        print("Your Pokémon evolved into Charizard.")
+        charzard()
+
+
+
+
+print("Welcome to the Pokemon game!")
+print("Your pokemon is charmander!")
+charmander()
+
+while True:
+    print("\nDay:", day)
+    print("Choose an activity:")
+    print("1. Train")
+    print("2. Gym Battle")
+    print("3. Final Boss Battle")
+    print("4. Rest / Pokémon Info")
+    print("5. Quit")
+
+    choice = input("Activity for the day: ")
+
+    if choice == "1":
+        train()
+        evolve()
+    elif choice == "2":
+        gym_battle()
+        evolve()
+    elif choice == "3":
+        final_boss_battle()
+    elif choice == "4":
+        rest()
+    elif choice == "5":
+        print("Exiting game.")
+        break
+    else:
+        print("Please choose a valid option (1–4).")
+        continue
+
+day = day + 1
+
+
